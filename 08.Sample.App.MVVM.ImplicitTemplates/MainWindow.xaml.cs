b211abe3351118733885452a19b35687fca342b2
Cristóbal Tello <cristobal.tello@outlook.com>
2019-05-02 23:19:21 +0200

    Add 08 project into solution

---
+++ 08.Sample.App.MVVM.ImplicitTemplates/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _08.Sample.App.MVVM.ImplicitTemplates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 08.Sample.App.MVVM.ImplicitTemplates/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using _08.Sample.App.MVVM.ImplicitTemplates.Customers.ViewModels;
+
+namespace _08.Sample.App.MVVM.ImplicitTemplates
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new CustomerListViewModel();
+        }
+
+        public object CurrentViewModel {get; set;}
+
+    }
+
+}

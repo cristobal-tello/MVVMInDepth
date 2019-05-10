c37a99e8667f4b11abfe89e666c7f79798b7db47
Cristóbal Tello <cristobal.tello@outlook.com>
2019-05-11 00:45:37 +0200

    12 project

---
+++ 12.Sample.App.CustomBehaviourWithNotifyChange/ShowNotificationMessageBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Sample.App
+{
+    public class ShowNotificationMessageBehavior : Behavior<ContentControl>
+    {
+        public string Message
+        {
+            get
+            {
+                return (string)GetValue(MessageProperty);
+            }
+            set
+            {
+                SetValue(MessageProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ShowNotificationMessageBehavior), new PropertyMetadata(null, OnMessageChanged));
+
+        private static void OnMessageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = ((ShowNotificationMessageBehavior)d);
+            behavior.AssociatedObject.Content = e.NewValue;
+            behavior.AssociatedObject.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (s, e) =>
+              {
+                  AssociatedObject.Visibility = Visibility.Collapsed;
+              };
+        }
+    }
+}
